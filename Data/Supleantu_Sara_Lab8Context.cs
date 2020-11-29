14dd2863ec589d63e597cad7dae5a72c9e8ab699
SARA-MARIA SUPLEANTU <sara.supleantu@stud.ubbcluj.ro>
2020-11-29 19:03:58 +0200

    Add project files.

---
+++ Data/Supleantu_Sara_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Supleantu_Sara_Lab8.Models;
+
+namespace Supleantu_Sara_Lab8.Data
+{
+    public class Supleantu_Sara_Lab8Context : DbContext
+    {
+        public Supleantu_Sara_Lab8Context (DbContextOptions<Supleantu_Sara_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Supleantu_Sara_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Supleantu_Sara_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Supleantu_Sara_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Models/Publisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supleantu_Sara_Lab8.Models
+{
+    public class Publisher
+    {
+        public int ID { get; set; }
+
+        public string PublisherName { get; set; }
+
+        public ICollection<Book> Books { get; set; }
+    }
+}
